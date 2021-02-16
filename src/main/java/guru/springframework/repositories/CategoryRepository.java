41c6392bb719eba0fad42c39faff59588a667a8e
limmens <66556410+limmens@users.noreply.github.com>
2021-02-16 14:22:54 +0100

    commit and push

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,2 +1,10 @@
-package guru.springframework.repositories;public interface CategoryRepository {
+package guru.springframework.repositories;
+
+import guru.springframework.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,2 +1,10 @@
-package guru.springframework.repositories;public interface UnitOfMeasureRepository {
+package guru.springframework.repositories;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
